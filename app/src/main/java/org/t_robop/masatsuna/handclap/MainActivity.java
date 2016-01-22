e8b3de3029fcd7365b90280f3dd3f9955270e9e1
Masatsuna <CloQuespayl6>
2016-01-22 17:14:51 +0900

    ーボタンを拍手の画像に変更 ーインスタンスの解放を追加 ー注釈を追加

--- app/src/main/java/org/t_robop/masatsuna/handclap/MainActivity.java
+++ app/src/main/java/org/t_robop/masatsuna/handclap/MainActivity.java
@@ -5,28 +5,41 @@ import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    SoundPool soundPool;    //SoundPool型の変数
+    int soundId;            //音声ファイルをロードしたものを格納
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final SoundPool soundPool  = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        //soundPoolのインスタンスを生成
+        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        //new SoundPool(ロードするファイル数, ストリームタイプ（STREAM_MUSICは音楽音）,サンプリングレートのクオリティ(デフォルトは０));
 
-        final int soundId = soundPool.load(this, R.raw.clapping_short2, 1);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
-                                      public void onClick (View view) {
-                                          soundPool.play(soundId, 1, 1, 0, 0, 1.0F);
-                                      }
-                                  }
-
-        );
-
+        //音声ファイルをロード
+        soundId = soundPool.load(this, R.raw.clapping_short2, 1);
+        //sound.load(コンテキスト,ファイルのID,デフォルトは１);
     }
 
+    //インスタンスを解放（終了後もメモリを使用し続けるのを防ぐため）
+    @Override
+    protected void onPause() {  //onPauseはActivityがバックグラウンドに移動するときに呼び出される。
+        super.onPause();
+        //ロードしたファイルをアンロード
+        soundPool.unload(soundId);
+
+        //生成したインスタンスを解放
+        soundPool.release();
+    }
+
+    //ImageButtonが押されたときの処理内容
+    public void onClick (View view) {
+        //ロードした音声ファイルを再生
+        soundPool.play(soundId, 1, 1, 0, 0, 1.0F);
+        //soundPool.play(ロードしたファイルの変数名, 左の音量,右の音量,デフォルトは０,-1で無限ループ,0で１回再生,再生速度)
+    }
 }
